9b5f61b4d01ae5589b4b2871b576198a606cd024
Ricardo Sacramento <sacramentor@webeo.com>
2025-04-14 00:25:50 +0100

    feat: handling keypress, score

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,7 @@
 
 const rl = @import("raylib");
 const sp = @import("shapes.zig");
+const std = @import("std");
 
 pub fn main() anyerror!void {
     // const WINDOW_WIDTH = 2880;
@@ -9,12 +10,14 @@ pub fn main() anyerror!void {
     const SCREEN_WIDTH = 1920;
     const SCREEN_HEIGHT = 1080;
 
+    var isPaused: bool = false;
+
     var circle = sp.Circle.init(SCREEN_WIDTH, SCREEN_HEIGHT);
 
     rl.setConfigFlags(.{
         // .window_highdpi = true,
         .msaa_4x_hint = true,
-        // .vsync_hint = true, 
+        // .vsync_hint = true,
     });
     rl.initWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Leplage Eletronic");
     // rl.setWindowSize(SCREEN_WIDTH, SCREEN_HEIGHT);
@@ -23,21 +26,57 @@ pub fn main() anyerror!void {
     // var framesCounter: u32 = 0;
 
     rl.setTargetFPS(120);
-    while(!rl.windowShouldClose()) {
+    while (!rl.windowShouldClose()) {
         rl.beginDrawing();
         defer rl.endDrawing();
 
         rl.clearBackground(rl.Color.black);
 
+        if (rl.isKeyPressed(rl.KeyboardKey.space)) {
+            if (isPaused) {
+                isPaused = false;
+            } else {
+                isPaused = true;
+            }
+        }
 
-
-        //Update
-        // framesCounter+=1;
-        // if (((framesCounter/30)%2) == 1)
-        // {
+        if (!isPaused) {
             circle.update();
-            // framesCounter = 0;
-        // }
+        } else {
+            rl.drawText("game paused!", SCREEN_WIDTH - 150, 0, 20, rl.Color.white);
+        }
+
+        const keyPressed = rl.getKeyPressed();
+
+        switch (keyPressed) {
+            rl.KeyboardKey.a => { circle.checkInput('A'); circle.posReset(); continue; },
+            rl.KeyboardKey.b => { circle.checkInput('B'); circle.posReset(); continue; },
+            rl.KeyboardKey.c => { circle.checkInput('C'); circle.posReset(); continue; },
+            rl.KeyboardKey.d => { circle.checkInput('D'); circle.posReset(); continue; },
+            rl.KeyboardKey.e => { circle.checkInput('E'); circle.posReset(); continue; },
+            rl.KeyboardKey.f => { circle.checkInput('F'); circle.posReset(); continue; },
+            rl.KeyboardKey.g => { circle.checkInput('G'); circle.posReset(); continue; },
+            rl.KeyboardKey.h => { circle.checkInput('H'); circle.posReset(); continue; },
+            rl.KeyboardKey.i => { circle.checkInput('I'); circle.posReset(); continue; },
+            rl.KeyboardKey.j => { circle.checkInput('J'); circle.posReset(); continue; },
+            rl.KeyboardKey.k => { circle.checkInput('K'); circle.posReset(); continue; },
+            rl.KeyboardKey.l => { circle.checkInput('L'); circle.posReset(); continue; },
+            rl.KeyboardKey.m => { circle.checkInput('M'); circle.posReset(); continue; },
+            rl.KeyboardKey.n => { circle.checkInput('N'); circle.posReset(); continue; },
+            rl.KeyboardKey.o => { circle.checkInput('O'); circle.posReset(); continue; },
+            rl.KeyboardKey.p => { circle.checkInput('P'); circle.posReset(); continue; },
+            rl.KeyboardKey.q => { circle.checkInput('Q'); circle.posReset(); continue; },
+            rl.KeyboardKey.r => { circle.checkInput('R'); circle.posReset(); continue; },
+            rl.KeyboardKey.s => { circle.checkInput('S'); circle.posReset(); continue; },
+            rl.KeyboardKey.t => { circle.checkInput('T'); circle.posReset(); continue; },
+            rl.KeyboardKey.u => { circle.checkInput('U'); circle.posReset(); continue; },
+            rl.KeyboardKey.v => { circle.checkInput('V'); circle.posReset(); continue; },
+            rl.KeyboardKey.x => { circle.checkInput('X'); circle.posReset(); continue; },
+            rl.KeyboardKey.y => { circle.checkInput('Y'); circle.posReset(); continue; },
+            rl.KeyboardKey.z => { circle.checkInput('Z'); circle.posReset(); continue; },
+            else => {}
+        }
+
 
         circle.draw();
     }
--- src/shapes.zig
+++ src/shapes.zig
@@ -22,29 +22,30 @@ pub const Circle = struct {
     ring_inner_r: f32,
     ring_outer_r: f32,
     color: rl.Color,
-    letter: [1:0]u8,
+    letter: u8,
+    score: u32,
 
     pub fn init(x: u32, y: u32) Circle {
         const genNumber = generateRandomPos(0, 24) catch 0;
-        const letter = [1:0]u8{letters[genNumber]};
-
+        // const letter = [1:0]u8{letters[genNumber]};
         return Circle{ .screen_x = x, .screen_y = y, .center = rl.Vector2.init(
             @floatFromInt(generateRandomPos(0 + 50, x + 50) catch 0),
             @floatFromInt(generateRandomPos(0 + 50, y + 50) catch 0),
-        ), .radius = 50, .ring_inner_r = 150, .ring_outer_r = 155, .color = rl.Color.red, .letter = letter};
+        ), .radius = 50, .ring_inner_r = 150, .ring_outer_r = 155, .color = rl.Color.red, .letter = letters[genNumber], .score = 0 };
     }
 
     pub fn draw(self: *Circle) void {
         var buffer: [64]u8 = undefined;
-        const result = std.fmt.bufPrintZ(&buffer, "X: {d}, Y: {d}, Letter: {s}", .{ self.center.x, self.center.y, self.letter }) catch {
+        const result = std.fmt.bufPrintZ(&buffer, "X: {d}, Y: {d}, Letter: {c}, Score: {d}", .{ self.center.x, self.center.y, self.letter, self.score }) catch {
             std.debug.print("Formatting failed\n", .{});
             return;
         };
 
+        const letter = [1:0]u8{self.letter};
         rl.drawCircleV(self.center, self.radius, rl.Color.white);
         rl.drawCircleV(self.center, self.radius - 10, rl.Color.init(153, 36, 44, 255));
         rl.drawRing(self.center, self.ring_inner_r, self.ring_outer_r, 0, 360, 0, rl.Color.init(244, 244, 244, 255));
-        rl.drawText(&self.letter, @intFromFloat(self.center.x-8), @intFromFloat(self.center.y-12), 30, rl.Color.white);
+        rl.drawText(&letter, @intFromFloat(self.center.x - 8), @intFromFloat(self.center.y - 12), 30, rl.Color.white);
         rl.drawText(result, 0, 0, 20, rl.Color.white);
     }
 
@@ -57,10 +58,31 @@ pub const Circle = struct {
             self.ring_inner_r = 200;
             self.ring_outer_r = 205;
             const genNumber = generateRandomPos(0, 24) catch 0;
-            self.letter = [1:0]u8{letters[genNumber]};
+            self.letter = letters[genNumber];
         } else {
             self.ring_inner_r -= 1;
             self.ring_outer_r -= 1;
+        }
+    }
+
+    pub fn posReset(self: *Circle) void {
+        self.center = rl.Vector2.init(
+            @floatFromInt(generateRandomPos(0 + 50, self.screen_x - 50) catch 0),
+            @floatFromInt(generateRandomPos(0 + 50, self.screen_y - 50) catch 0),
+        );
+        self.ring_inner_r = 200;
+        self.ring_outer_r = 205;
+        const genNumber = generateRandomPos(0, 24) catch 0;
+        self.letter = letters[genNumber];
+    }
+
+    pub fn checkInput(self: *Circle, input: u8) void {
+        std.debug.print("{c}", .{input});
+
+        if (self.letter == input) {
+            if ((self.radius - 10) <= self.ring_inner_r and self.radius >= self.ring_outer_r) {
+                self.score += 5;
+            }
         }
     }
 };
